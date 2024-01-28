38069ec99f9301b32141494c3a921e3be5b02e25
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-01-28 11:12:49 -0300

    button pricipal hover effetc changed

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="w-16 h-16" src="{{ asset('storage/assets/logo.png') }}" alt="Imagem da tela de login">
+<img class="w-16 h-16" src="{{ asset('storage/assets/logo.png') }}" alt="Logo do mostruário">
