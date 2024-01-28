2166240b7a027876782b8a42190039e31e4ea695
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-01-28 14:51:21 -0300

    add skeleton

--- resources/views/livewire/placeholders/skeleton.blade.php
+++ resources/views/livewire/placeholders/skeleton.blade.php
@@ -1,11 +1,8 @@
 <div>
-    <div role="status" class="max-w-sm animate-pulse">
-        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+    <div role="skeleton" class="w-full animate-pulse p-6">
+        <div class="w-full flex justify-between items-center">
+            <div class="h-8 bg-gray-200 rounded-sm dark:bg-gray-700 w-1/4"></div>
+        </div>
         <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-        <span class="sr-only">Loading...</span>
     </div>
 </div>
