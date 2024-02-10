09636192ac4260201db02b75712e1d0c1905985c
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-02-10 12:04:03 -0300

    filesystems link public_path only /

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('public/storage'),
+            'root' => storage_path('/'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
