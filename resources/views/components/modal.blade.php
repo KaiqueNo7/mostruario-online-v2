26b2a0c8830ae5156c88c6f4c01654cc76e1cb7c
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-02-11 18:32:27 -0300

    modal adjustes in mobile

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -3,7 +3,6 @@
 {{ $show }}
 @if($show === true)
     <div
-        wire:click='closeModal'
         class="fixed inset-0 transform transition-all z-50"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
@@ -12,12 +11,10 @@
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
     >
-        <div class="absolute inset-0 bg-gray-500 opacity-75">
+        <div wire:click='closeModal' class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        <div class="fixed w-full max-w-sm max-h-full top-1/2 left-1/2 p-4 transition-opacity z-10 duration-300 ease-out rounded-sm transform -translate-x-1/2 -translate-y-1/2 bg-white overflow-auto shadow opacity-100 visible">
+            {{ $slot }}
         </div>
-            <div class="fixed w-full max-w-sm max-h-full top-1/2 left-1/2 p-4 transition-opacity z-10 duration-300 ease-out rounded-sm transform -translate-x-1/2 -translate-y-1/2 bg-white overflow-auto shadow opacity-100 visible">
-                {{ $slot }}
-            </div>
-       
     </div>
     
 @else
