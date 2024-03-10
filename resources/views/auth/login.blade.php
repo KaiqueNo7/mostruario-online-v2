cf23c34492ef0907d7caff547c4a760b392070f8
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-03-10 01:41:03 -0300

    button google login 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
 
     <a href="{{ route('login.google') }}" class="px-4 py-2 my-4 border flex justify-center items-center gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
         <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
-        <span>Login com Google</span>
+        <span>Login com o Google</span>
     </a>
 
 
