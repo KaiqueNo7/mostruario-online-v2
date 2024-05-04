d02efcec7eaeb4f0a78cb4e62deeb4da260614a1
Kaique Nocetti <kaiquenocetti07@gmail.com>
2024-05-04 12:59:26 -0300

    buttons css adjustes front end

--- resources/views/livewire/button-principal.blade.php
+++ resources/views/livewire/button-principal.blade.php
@@ -3,24 +3,24 @@
         <input type="hidden" value="{{ asset('/mo/' .str_replace(' ', '-', strtolower(Auth::user()->name)) ) }}" id="linkInput">
      
             <div class="group relative flex justify-center {{ $opacity }} {{ $scale }} transition duration-300 ease-out rounded-full my-1">
-                <span id="copiedMessage" class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300">Copiar link</span>
+                <span id="copiedMessage" class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300 rounded-xl shadow-md">Copiar link</span>
                 <button onclick="copyToClipboard();" class="p-5 rounded-full w-8 h-8 border-none flex justify-center items-center bg-green-500 hover:bg-white text-white hover:text-green-500 text-base cursor-pointer drop-shadow-md transition ease-in-out duration-300"><i class="fa-regular fa-copy"></i></button>
             </div>
             
             @if($categories > 0)
             <div class="group relative flex justify-center {{ $opacity }} {{ $scale }} transition duration-300 ease-out rounded-full my-1">
-                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300">Adicionar produto</span>
+                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300 rounded-xl shadow-md">Adicionar produto</span>
                 <button wire:click="showModalProduct" class="p-5 rounded-full w-8 h-8 border-none flex justify-center items-center bg-green-500 hover:bg-white text-white hover:text-green-500 text-base cursor-pointer drop-shadow-md transition ease-in-out duration-300"><i class="fa-solid fa-box-open"></i></button>
             </div>
 
             <div class="group relative flex justify-center {{ $opacity }} {{ $scale }} transition duration-300 ease-out rounded-full my-1">
-                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300">Adicionar vários produtos</span>
+                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300 rounded-xl shadow-md">Adicionar vários produtos</span>
                 <button wire:click="showModalVariousProducts" class="p-5 rounded-full w-8 h-8 border-none flex justify-center items-center bg-green-500 hover:bg-white text-white hover:text-green-500 text-base cursor-pointer drop-shadow-md transition ease-in-out duration-300"><i class="fa-solid fa-boxes-stacked"></i></button>
             </div>
             @endif
 
             <div class="group relative flex justify-center {{ $opacity }} {{ $scale }} transition duration-300 ease-out rounded-full my-1">
-                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300">Adicionar categoria</span>
+                <span class="absolute pointer-events-none right-12 whitespace-nowrap p-2 bg-white text-gray-600 font-medium text-sm transition ease-in-out duration-300 rounded-xl shadow-md">Adicionar categoria</span>
                 <button wire:click="showModalCategory" class="p-5 rounded-full w-8 h-8 border-none flex justify-center items-center bg-green-500 hover:bg-white text-white hover:text-green-500 text-base cursor-pointer drop-shadow-md transition ease-in-out duration-300"><i class="fa-solid fa-tags"></i></button>
             </div>
         <button class="w-16 h-16 mt-2 text-4xl transition ease-int-out duration-300 flex items-center justify-center drop-shadow-md bg-green-500 hover:bg-white text-white hover:text-green-500 rounded-full {{ $rotateClass }}" wire:click="moreOptions"><i class="fa-solid fa-plus"></i></button>
